15f75b685fcbf06142f18d109af284a1aeae0d88
Wagner Resta <wagner.silveira@gwdesenvolvimento.com.br>
2021-02-06 22:45:13 -0300

    Telas de Navegação

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Inicial from './src/views/inicial/Inicial';
+import Inicial from './src/views/Inicial/Inicial';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Colecao from './src/views/Colecao/Colecao';
 import Item from './src/views/Item/Item';
+import Perfil from './src/views/Perfil/Perfil';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 const Stack = createStackNavigator();
+const Tab   = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
-export default function App() {
+function rotasStack(){
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
+    <Stack.Navigator initialRouteName="Inicial">
         <Stack.Screen 
           name="Inicial" 
           component={Inicial}
@@ -46,7 +50,40 @@ export default function App() {
             }
           }
         />
-      </Stack.Navigator>
+        </Stack.Navigator>
+  );
+}
+
+function rotasDrawer(){
+  return (
+    <Drawer.Navigator initialRouteName="Inicial">
+      <Drawer.Screen name="Inicial" component={rotasStack}/>
+      <Drawer.Screen name="Colecao" component={Colecao}/>
+      <Drawer.Screen name="Item" component={Item}/>
+      <Drawer.Screen name="Perfil" component={Perfil}/>
+    </Drawer.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      
+      <Tab.Navigator
+        tabBarOptions={
+          {
+            labelStyle:{fontSize:24},
+            style:{backgroundColor:'#14417b'},
+            activeTintColor:'#FFF'
+          }
+        }
+      >
+        
+        <Tab.Screen name="Inicial" component={rotasDrawer}/>
+        <Tab.Screen name="Perfil" component={Perfil}/>
+        
+      </Tab.Navigator>
+      
     </NavigationContainer>
   );
 }
@@ -61,10 +98,4 @@ const styles = StyleSheet.create({
   screenItem:{
     backgroundColor:'#d1d1d1'
   },
-  /*container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },*/
 });
